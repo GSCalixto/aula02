7e6ff22a222191cac62aadc89cc1d39c350e0728
Gabriel <gabriel_calixto.gsc@hotmail.com>
2018-10-23 11:27:18 -0300

    aula02
    TÁ OK!?

---
+++ aula5ex2js.js
@@ -0,0 +1,9 @@
+function c1(){
+	document.getElementById('Prod').style.display = "block";
+	document.getElementById('Form').style.display = "none";
+}
+
+function c2() {
+	document.getElementById('Form').style.display = "block";
+	document.getElementById('Prod').style.display = "none";
+}
---
+++ aula5js.js
@@ -0,0 +1,9 @@
+function c1(){
+	document.getElementById('conteudo1').style.display = "block";
+	document.getElementById('conteudo2').style.display = "none";
+}
+
+function c2() {
+	document.getElementById('conteudo1').style.display = "none";
+	document.getElementById('conteudo2').style.display = "block";
+}
